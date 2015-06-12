90c0c03bcc613b8a2e615a88296afda134b898cf
Les Aker <me@lesaker.org>
2015-06-12 09:59:16 -0400

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,16 @@
+# == Class: chrome
+#
+# Full description of class chrome here.
+#
+# === Parameters
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#
+class chrome (
+  $package = 'chrome-dev-halyard'
+) {
+  package { $package:
+    provider => 'brewcask'
+  }
+}
